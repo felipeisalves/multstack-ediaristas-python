2e3e815eca9feb14727c1362f89120ec8f110425
felipe <felipeme@outlook.com>
2022-05-11 22:24:10 -0300

    CRUD de serviços pronto

---
+++ administracao/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import redirect, render
+from .forms import ServiceForm
+from .models import Servico
+# Create your views here.
+
+def cadastrar_servico(request):
+    if request.method == "POST":
+        form_servico = ServiceForm(request.POST)
+        if form_servico.is_valid():
+            form_servico.save()
+            return redirect('listar_servicos')
+    else:
+        form_servico = ServiceForm()
+    return render(request, 'servicos/form_servico.html', {"form_servico": form_servico})
+
+def listar_servicos(request):
+    servicos = Servico.objects.all()
+    return render(request, 'servicos/lista_servicos.html', {'servicos': servicos})
+
+def editar_servico(request, id):
+    # Igual um select, where id = id
+    servico = Servico.objects.get(id=id)
+    # tras todos os campos já preenchidos
+    form_servico = ServiceForm(request.POST or None, instance=servico)
+    if form_servico.is_valid():
+        form_servico.save()
+        return redirect('listar_servicos')
+    return render(request, 'servicos/form_servico.html', {'form_servico': form_servico})
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Projeto e-diaristas
+
+### Instalando o projeto
+
+### Clonar o projeto
+`git clone url`
+
+### Instalar dependências
+`pip install -r requirements.txt`
+
+### Alterar configurações do BD no arquivo settings.py
+```
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nome_do_db',
+        'HOST': '_do_db_db',
+        'PORT': porta_do_db,
+        'USER': 'usuario_do_db',
+        'PASSWORD': 'senha_db'
+    }
+}
+```
+
+### Migrar banco de dados
+`python manage.py migrate`
+
+### Iniciar o servidor
+`python manage.py runserver`
